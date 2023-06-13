b281e5e2f2ff5a90c5ea98887151a4f259978330
Darafty7 <aacantero4@misena.edu.co>
2023-06-13 17:54:08 -0500

    Vercel2.0

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,6 @@ export const DB_PORT = process.env.DB_PORT || 5432
 export const DB_USER = process.env.DB_USER || 'postgres'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '0000'
 export const DB_DATABASE = process.env.DB_DATABASE || '1prueba'
-
+export  const DB_HOST = process.env.DB_HOST || 'localhost'
 
 
